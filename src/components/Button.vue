ee0d8185332ec77ef29bdf3f8d64c3b5bce910c7
Mikhail Khlevin <m.khlevin@gmail.com>
2022-11-27 20:55:55 +0400

    initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="btn" @click="onClick()" :style="{ background: color }">
+    {{description}}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    description: String,
+    color: String
+  },
+  methods: {
+    onClick() {
+      this.$emit('btn-click')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+      :key="task.id"
+      class="task-div"
+      v-for="task in tasks"
+  >
+    <Task
+        @toggle-reminder="$emit('toggle-reminder', task.id)"
+        @delete-task="$emit('delete-task', task.id)"
+        :task="task"
+    />
+  </div>
+</template>
+
+<script>
+import Task from '../components/Task.vue'
+
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array
+  },
+  components: {
+    Task
+  },
+  emits: ['delete-task', 'toggle-reminder'],
+}
+</script>
+
+<style scoped>
+  .task-div {
+    margin-top: 10px;
+  }
+</style>
